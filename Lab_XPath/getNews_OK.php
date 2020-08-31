f9bdf87f7176c315f889d8ec217f15d43c362abc
ryu <ryu64j@gmail.com>
2020-08-31 17:05:56 +0800

    Xpath test weather

--- Lab_XPath/getNews_OK.php
+++ Lab_XPath/getNews_OK.php
@@ -5,7 +5,7 @@ header("content-type: text/html; charset=utf-8");
 $ch = curl_init();
 
 // 2. 設定 / 調整參數
-curl_setopt($ch, CURLOPT_URL, "https://www.taipower.com.tw/TC/news2.aspx?mid=225");
+curl_setopt($ch, CURLOPT_URL, "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-EAD35A23-4827-4F86-85CF-E4898711F30C&elementName=PoP");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 
@@ -13,18 +13,36 @@ curl_setopt($ch, CURLOPT_HEADER, 0);
 $pageContent = curl_exec($ch);
 
 // 4. 關閉與釋放資源
-curl_close($ch);
+// var_dump($a);
+// foreach ($a as $key => $value){
+// 	echo $key, "=>", $value, "<br>";
+// }
 
-$doc = new DOMDocument();
-libxml_use_internal_errors(true);
-$doc->loadHTML($pageContent);
-
-$xpath = new DOMXPath($doc);
-$entries = $xpath->query('//*[@id="news_box3"]/div[2]/ul/li');
-foreach ($entries as $entry) 
-{
-    $title = $xpath->query("./a/div/h3", $entry);
-    echo "Title：" . $title->item(0)->nodeValue . "<br>";
+//  echo htmlspecialchars($a);
+$a = json_decode($pageContent,true);
+foreach ($a['records']['location'] as $i) {
+    echo $i["locationName"]."降雨率";
+    for($count=0;$count<3;$count++){
+        echo $i["weatherElement"][0]["time"][$count]["parameter"]["parameterName"];
+    }
+    echo "<br>";
+   
 }
+
+
+
+
+// echo $a
+// $doc = new DOMDocument();
+// libxml_use_internal_errors(true); //有錯誤繼續  
+// $doc->loadHTML($pageContent);
+
+// $xpath = new DOMXPath($doc);
+// $entries = $xpath->query('records');
+// foreach ($entries as $entry) 
+// {
+//     $title = $xpath->query("records", $entry);
+//     echo "Title：" . $title->item(0)->nodeValue . "<br>";
+// }
 
 ?>
